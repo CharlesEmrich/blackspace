c50213bb3d8f83f7900ddf30fad5adfe4e7dc014
Alyssa Moody and Charles Emrich <alyssa+charles@>
2017-05-15 14:35:11 -0700

    add prototype method to display ciphertext to cipherlength of n

--- js/scripts.js
+++ js/scripts.js
@@ -30,27 +30,43 @@ Cipher.prototype.encodeChar = function (char, shift) {
 var ourCipher = new Cipher();
 /// User Interface ///
 $(function() {
+  Cipher.prototype.displayCipherText = function (array) {
+    $("#output").empty();
+    var printThis = "";
+    for (var i = 0; i < array.length; i++) {
+      printThis = "";
+      for (var ii = 0; ii < array[i].length; ii++) {
+        if (this.alpha.indexOf(array[i][ii]) !== -1 || this.numbers.indexOf(array[i][ii]) !== -1) {
+          printThis += array[i][ii];
+        }
+      }
+      $("#output").append("<div class='singleChar'>" + printThis + "</div>");
+    }
+  };
+
   $("select").change(function() {
     ourCipher.cipherLength = parseInt($(this).val());
-    console.log(ourCipher);
-    //TODO: re-parse output div using new cipherLength.
+    ourCipher.displayCipherText(userString);
   });
 
+  var userString = []
   $("textarea").keypress(function(event) {
-    var encodedChar = ourCipher.encodeChar(event.key.toLowerCase(), event.shiftKey);
-    console.log(encodedChar);
+    var encodedArr = ourCipher.encodeChar(event.key.toLowerCase(), event.shiftKey);
+    userString.push(encodedArr);
+    console.log(userString);
+    ourCipher.displayCipherText(userString);
 
-    if (encodedChar.length > 0) {
-      $("#output").append(
-        "<div class='singleChar'>" +
-        encodedChar[0] + "<span class='shifted'>" + encodedChar[1] + "</span>" +
-        "</div>"
-      );
-    } else {
-      $("#output").append(
-        "<div class='singleChar'>" +
-        event.key +
-        "</div>")
-    }
+    // if (encodedChar.length > 0) {
+    //   $("#output").append(
+    //     "<div class='singleChar'>" +
+    //     encodedChar[0] + "<span class='shifted'>" + encodedChar[1] + "</span>" +
+    //     "</div>"
+    //   );
+    // } else {
+    //   $("#output").append(
+    //     "<div class='singleChar'>" +
+    //     event.key +
+    //     "</div>")
+    // }
   });
 });
