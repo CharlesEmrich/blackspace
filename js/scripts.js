69d3389eb54477e2864389884f4aa427c4b51c6d
Alyssa Moody and Charles Emrich <alyssa+charles@>
2017-05-15 10:06:53 -0700

    let cipher replace letters with their nearest neighbors

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,19 @@
 /// Business ///
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+var numbers  = "1234567890".split("");
 
 /// User Interface ///
 $(function() {
     $("textarea").keyup(function() {
-      var outputChar = $(this).val()[$(this).val().length - 1]
+      var inputChar = $(this).val()[$(this).val().length - 1]
+      var outputChar = alphabet[alphabet.indexOf(inputChar) - 1] + alphabet[alphabet.indexOf(inputChar) + 1];
+      if (inputChar === "a") {
+        outputChar = "zb";
+      } else if (inputChar === "z") {
+        outputChar = "ya";
+      } else if (alphabet.indexOf(inputChar) === -1) {
+        outputChar = inputChar;
+      }
       $("#output").text(outputChar);
 
     });
