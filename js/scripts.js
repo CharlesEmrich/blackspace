e503cd0d448294bde38e0e0614a918e45501ef60
Alyssa Moody and Charles Emrich <alyssa+charles@>
2017-05-15 11:35:56 -0700

    refactor outputChar as outputArr and keyup as keypress, also account for capital letters and numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,53 @@
 /// Business ///
+//NOTE: add handling for capitals.
 var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
-var numbers  = "1234567890".split("");
+//NOTE: add handling for 0, 1.
+var numbers  = "0123456789".split("");
 
 /// User Interface ///
 $(function() {
-    $("textarea").keyup(function() {
-      var inputChar = $(this).val()[$(this).val().length - 1]
-      var outputChar = alphabet[alphabet.indexOf(inputChar) - 1] + alphabet[alphabet.indexOf(inputChar) + 1];
+  $("textarea").keypress(function(event) {
+    var inputChar = event.key.toLowerCase();
+    var outputArr = [];
+
+    //NOTE: Dry this into a single function?
+    if (alphabet.indexOf(inputChar) !== -1) {
       if (inputChar === "a") {
-        outputChar = "zb";
+        outputArr.push("z","b");
       } else if (inputChar === "z") {
-        outputChar = "ya";
-      } else if (alphabet.indexOf(inputChar) === -1) {
-        outputChar = inputChar + " ";
+        outputArr.push("y","a");
+      } else {
+        outputArr.push(alphabet[alphabet.indexOf(inputChar) - 1], alphabet[alphabet.indexOf(inputChar) + 1]);
       }
+    }
+
+    if (event.shiftKey) {
+      outputArr = outputArr.map(function(letter) {
+        return letter.toUpperCase();
+      })
+    }
+
+    if (numbers.indexOf(inputChar) !== -1) {
+      if (inputChar === "0") {
+        outputArr.push("9", "1");
+      } else if (inputChar === "9") {
+        outputArr.push("8", "0");
+      } else {
+        outputArr.push(numbers[numbers.indexOf(inputChar) - 1], numbers[numbers.indexOf(inputChar) + 1]);
+      }
+    }
+
+    if (outputArr.length > 0) {
       $("#output").append(
         "<div class='singleChar'>" +
-        outputChar[0] + "<span class='shifted'>" + outputChar[1] + "</span>" +
+        outputArr[0] + "<span class='shifted'>" + outputArr[1] + "</span>" +
         "</div>"
       );
-    });
-});
+    } else {
+      $("#output").append(
+        "<div class='singleChar'>" +
+        event.key +
+        "</div>")
+    }
+  });
+}
